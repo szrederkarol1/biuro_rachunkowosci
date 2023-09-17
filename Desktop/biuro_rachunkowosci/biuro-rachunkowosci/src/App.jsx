4ec3c164a5bccea27fc0110eaa35a9300347e710
Karol <szrederkarol1@gmail.com>
2023-09-17 19:51:01 +0200

    Completed layout

--- Desktop/biuro_rachunkowosci/biuro-rachunkowosci/src/App.jsx
+++ Desktop/biuro_rachunkowosci/biuro-rachunkowosci/src/App.jsx
@@ -3,10 +3,12 @@ import Header from "./components/header/Header";
 import AboutUs from "./components/about_us/AboutUs";
 import Offers from "./components/offers/Offers";
 import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 import "./components/header/Header.scss";
 import "./components/offers/Offers.scss";
 import "./components/about_us/AboutUs.scss";
 import "./components/contact/Contact.scss";
+import "./components/footer/Footer.scss";
 import "./height";
 
 const App = () => {
@@ -16,6 +18,7 @@ const App = () => {
       <AboutUs />
       <Offers />
       <Contact />
+      <Footer />
     </div>
   );
 };
