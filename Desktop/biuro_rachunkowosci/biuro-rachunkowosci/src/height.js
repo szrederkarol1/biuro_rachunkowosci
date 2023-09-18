41d61e91cff47093424547d41d5add78628fb3a7
Karol Szreder <116760642+szrederkarol1@users.noreply.github.com>
2023-09-18 18:22:14 +0200

    Delete Desktop/biuro_rachunkowosci/biuro-rachunkowosci directory

--- Desktop/biuro_rachunkowosci/biuro-rachunkowosci/src/height.js
+++
@@ -1,4 +0,0 @@
-const wysokoscWidoku = window.innerHeight;
-console.log(`Wysokość widoku wynosi ${wysokoscWidoku}px`);
-const szerokoscWidoku = window.innerWidth;
-console.log(`Szerokość widoku wynosi ${szerokoscWidoku}px`);
