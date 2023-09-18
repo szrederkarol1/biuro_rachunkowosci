22e8460fc185a2ffc79a33fc3b384576f19579fb
Karol Szreder <szrederkarol1@gmail.com>
2023-09-18 18:43:26 +0200

    New directory with project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.scss";
+import Header from "./components/header/Header";
+import AboutUs from "./components/about_us/AboutUs";
+import Offers from "./components/offers/Offers";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import "./components/header/Header.scss";
+import "./components/offers/Offers.scss";
+import "./components/about_us/AboutUs.scss";
+import "./components/contact/Contact.scss";
+import "./components/footer/Footer.scss";
+import "./height";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <AboutUs />
+      <Offers />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <>
+      <section className="contact-content">
+        <div className="contact-container">
+          <div className="form">
+            <div className="email-container">
+              <label htmlFor="email">Twój Adres e-mail</label>
+              <input
+                type="email"
+                className="email-input"
+                placeholder="email@email.com"
+              ></input>
+            </div>
+            <div className="message-container">
+              <label htmlFor="message">Wiadomość</label>
+              <textarea id="message"></textarea>
+            </div>
+            <button className="btn-form">Wyślij</button>
+          </div>
+          <div className="contact-details">
+            <h2>Skontaktuj się z nami</h2>
+            <div className="details">
+              <div className="adress"><div></div>
+                <h6>Adres</h6>
+                <p>84-215 Gowino ul. Cicha 13/1</p>
+              </div>
+              <div className="email">
+                <h6>Adres e-mail</h6>
+                <p>biuro@rachunkowosckms.pl</p>
+              </div>
+              <div className="number">
+                <h6>Nr telefonu</h6>
+                <p>+48 669 753 700</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Contact;
