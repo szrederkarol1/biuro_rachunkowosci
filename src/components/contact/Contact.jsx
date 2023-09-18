5ed4f009335c5a9afc517d8055302ebb14ea7751
Karol Szreder <szrederkarol1@gmail.com>
2023-09-18 20:58:47 +0200

    Layout for desktop done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,23 +7,31 @@ const Contact = () => {
         <div className="contact-container">
           <div className="form">
             <div className="email-container">
-              <label htmlFor="email">Twój Adres e-mail</label>
+              <label htmlFor="email" alt="Twój adres e-mail" for="email">
+                Twój Adres e-mail
+              </label>
               <input
                 type="email"
                 className="email-input"
                 placeholder="email@email.com"
+                name="email"
+                id="email"
               ></input>
             </div>
             <div className="message-container">
-              <label htmlFor="message">Wiadomość</label>
-              <textarea id="message"></textarea>
+              <label htmlFor="message" alt="Wiadomość" for="message">
+                Wiadomość
+              </label>
+              <textarea id="message" name="message"></textarea>
             </div>
-            <button className="btn-form">Wyślij</button>
+            <button type="submit" className="btn-form">
+              Wyślij
+            </button>
           </div>
           <div className="contact-details">
             <h2>Skontaktuj się z nami</h2>
             <div className="details">
-              <div className="adress"><div></div>
+              <div className="adress">
                 <h6>Adres</h6>
                 <p>84-215 Gowino ul. Cicha 13/1</p>
               </div>
