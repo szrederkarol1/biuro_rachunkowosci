1d3fd6269563c1f9652803dc759a87c70c9ff28b
Karol Szreder <szrederkarol1@gmail.com>
2023-11-22 19:51:03 +0100

    fix firebase

--- public/index.jsx
+++ public/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Przykładowe miejsce użycia Firebase, np. w pliku main.js
+import "./firebase"; // importuje wcześniej stworzony plik z konfiguracją
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
