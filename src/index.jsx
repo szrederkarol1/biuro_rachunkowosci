d2b459871a50511039763b2ab240880c15176138
Karol Szreder <szrederkarol1@gmail.com>
2023-11-27 20:07:04 +0100

    Finish fix

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// Przykładowe miejsce użycia Firebase, np. w pliku main.js
-import "../.firebase"; // importuje wcześniej stworzony plik z konfiguracją
+import "../.firebase"; 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
