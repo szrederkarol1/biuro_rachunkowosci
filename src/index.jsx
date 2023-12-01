b92523aa2c29c7715a11708a8971ace037e4dc96
Karol Szreder <szrederkarol1@gmail.com>
2023-12-01 11:39:01 +0100

    Fix head HTML

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "../.firebase"; 
+// import "../.firebase"; 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
